b41256a34f94d6acbe8e77a8193076b644f6b1d6
nskrzypczyk <skrzypczyk.noah@gmail.com>
2019-11-01 20:01:38 +0100

    [React] Standortuebersicht mit Markern

--- uebersicht/src/App.js
+++ uebersicht/src/App.js
@@ -6,13 +6,16 @@ import Content from "./content.component";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Die LOKALISATOREN</header>
-      <h1 className="h">Gespeicherte Daten</h1>
-      <p className="info">
-        Info: Bei Klick auf einen Zeitstempel wird zu Google Maps weitergeleitet
-        ;)
-      </p>
-      <hr />
+      <header className="App-header">
+        Die LOKALISATOREN <h1 className="h">Gespeicherte Daten</h1>
+      </header>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+        crossorigin=""
+      />
+
       <Content />
     </div>
   );
--- uebersicht/src/content.component.js
+++ uebersicht/src/content.component.js
@@ -1,32 +1,70 @@
 import React from "react";
 import axios from "axios";
 import Card from "./card.component";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default class Content extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pos: []
+      pos: [10]
     };
+    this.position = [51.505, -0.09];
   }
-  componentDidMount() {
+
+  componentWillMount() {
     axios
       .get("http://localhost:80/api/position/all")
       .then(response => {
         console.log("DATA: ", response.data);
         this.setState({
-          pos: response.data
+          pos: response.data,
+          zoom: 17
         });
       })
       .catch(err => console.log(err));
+    console.log(this.state.pos[0]);
+    this.render();
   }
 
   render() {
     return (
-      <div className="grid">
-        {this.state.pos.map(item => (
-          <Card item={item} />
-        ))}
+      <div className="Container">
+        <Map
+          center={
+            this.state.pos != undefined
+              ? { lat: this.state.pos[0].lat, lng: this.state.pos[0].long }
+              : { lat: 0, lng: 0 }
+          }
+          zoom={this.state.zoom}
+          style={{
+            height: 400,
+            width: "100%",
+            overflow: "hidden",
+            marginBottom: "1%",
+            border: "1px solid #ccc",
+            boxShadow: "2px 2px 6px 0px rgba(0, 0, 0, 0.3)"
+          }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          {this.state.pos.map(item =>
+            item.long != undefined ? (
+              <Marker position={{ lat: item.lat, lng: item.long }}>
+                <Popup>
+                  <span>{item.timeStamp}</span>
+                </Popup>
+              </Marker>
+            ) : null
+          )}
+        </Map>
+        <div className="grid">
+          {this.state.pos.map(item => (
+            <Card item={item} />
+          ))}
+        </div>
       </div>
     );
   }
