4d53d970b42789fa6d09811a801c2705ff555af8
Tim Poerschke <info@coder-tim.de>
2019-11-24 16:18:45 +0100

    [TASK] GUI erstellt

--- Praktikum1/app/src/main/java/com/example/praktikum1/StatisticsActivity.java
+++ Praktikum1/app/src/main/java/com/example/praktikum1/StatisticsActivity.java
@@ -3,12 +3,26 @@ package com.example.praktikum1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    // GUI-ELEMENTS
+    private Spinner routeSelector;
+    private TextView statusTextView;
+    // GraphView
+    private GraphView cdfGraph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        routeSelector = findViewById(R.id.routeSelector);
+        cdfGraph = findViewById(R.id.cdfGraph);
+        statusTextView = findViewById(R.id.statusTextView);
     }
 }
