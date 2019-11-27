f827943bf734073feb9300ddc78c363e2179169e
Tim Poerschke <info@coder-tim.de>
2019-11-27 17:28:47 +0100

    [TASK] Layout der TraitTable angepasst

--- Praktikum1/app/src/main/java/com/example/praktikum1/statistics/TraitTable.java
+++ Praktikum1/app/src/main/java/com/example/praktikum1/statistics/TraitTable.java
@@ -1,8 +1,10 @@
 package com.example.praktikum1.statistics;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -30,13 +32,19 @@ public class TraitTable {
         labels.add(Utils.TYPE_LM_GPS);
 
         table = new TableLayout(context);
-        TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(
-                TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
-        tableParams.topMargin = 20;
+        table.setColumnStretchable(1, true);
+        LinearLayout.LayoutParams tableParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        tableParams.topMargin = 30;
         table.setLayoutParams(tableParams);
 
         TableLayout.LayoutParams tableRowParams = new TableLayout.LayoutParams(
                 TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
+
+        // Ist für das TextView, welches den Title anzeigt. Dieses bekommt eine colpsan von 2
+        TableRow.LayoutParams tableTitleParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        tableTitleParams.span = 2;
 
         // Titel basteln
         titleRow = new TableRow(context);
@@ -45,16 +53,21 @@ public class TraitTable {
         title.setAllCaps(true);
         title.setTypeface(null, Typeface.BOLD);
         title.setText(titleContent);
+        title.setLayoutParams(tableTitleParams);
         titleRow.addView(title);
         table.addView(titleRow);
 
         // Für jeden TYPE (flp_high, ...) eine Zeile rendern
         for(String label : labels) {
             TableRow tr = new TableRow(context);
-            titleRow.setLayoutParams(tableRowParams);
+            tr.setLayoutParams(tableRowParams);
+
+            TableRow.LayoutParams labelLayout = new TableRow.LayoutParams();
+            labelLayout.rightMargin = 30;
 
             TextView tv = new TextView(context);
             tv.setText(label);
+            tv.setLayoutParams(labelLayout);
             tr.addView(tv);
 
             TextView valueView = new TextView(context);
